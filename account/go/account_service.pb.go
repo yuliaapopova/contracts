1d7e7595713416f554802bf4acf62dc0774985ef
Yulia Popova <yuliaappopova@gmail.com>
2025-08-18 18:36:49 +0300

    fix

--- account/go/account_service.pb.go
+++ account/go/account_service.pb.go
@@ -7,15 +7,14 @@
 package account
 
 import (
-	reflect "reflect"
-	sync "sync"
-	unsafe "unsafe"
-
 	_go "github.com/yuliaapopova/contracts/pagination/go"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
 )
 
 const (
